8abd51194f1873ebf37d88c1366697f62d083773
Benjamin Dirix <BenjaminDirix@hotmail.com>
2019-04-12 13:25:10 +0200

    Labels

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -11,6 +11,7 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
