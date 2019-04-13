28a5b02a7aac509d73e94d47ccd98fce407e7b50
Ben Dirix <benjamindirix@hotmail.com>
2019-04-13 22:25:22 +0200

    Data Transfer Object

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        [Required(ErrorMessage = "Please enter the customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Min18YearsIfAMember]
+        public DateTime? BirthDate { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public short MembershipTypeId { get; set; }
+    }
+}
