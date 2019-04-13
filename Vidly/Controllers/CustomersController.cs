61eb52c70af92bf8d781c8d80acb7aae92cd1916
Ben Dirix <benjamindirix@hotmail.com>
2019-04-13 13:32:28 +0200

    validation summary

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -45,6 +45,7 @@ namespace Vidly.Controllers
             var membershipTypes = _dbContext.MembershipTypes.ToList();
             var ViewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes,
             };
             return View("CustomerForm", ViewModel);
