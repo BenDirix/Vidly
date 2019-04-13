a7a1dadb66dada06a31fdde850f569baa14001cf
Ben Dirix <benjamindirix@hotmail.com>
2019-04-13 12:43:09 +0200

    Custom Validation

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,16 +9,17 @@ namespace Vidly.Models
     public class Customer{
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter the customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
+        [Min18YearsIfAMember]
         [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
 
-        [Display(Name= "Membership Type")]
+        [Display(Name= "Membership Type")]        
         public short MembershipTypeId { get; set; }
     }
 }
